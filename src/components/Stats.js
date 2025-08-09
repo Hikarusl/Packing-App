3c7d7cf2105a1880efc828274eb494122cfd1341
Zabarina <svetoch.z.2011@gmail.com>
2025-08-09 15:29:45 +0300

    Initial commit

---
+++ src/components/Stats.js
@@ -0,0 +1,14 @@
+export default function Stats({items}) {
+  const numItems = items.length
+  const numPacked = items.reduce((acc, item)=> (item.packed) ? acc+1 : acc,0)
+  const percentage = Math.round(numPacked/numItems * 100)
+
+  return (
+    <footer className="stats">
+      <em>
+        {percentage === 100
+          ? "You got everything! Ready to go ✈️"
+          : ` 💼 You have ${numItems} items on your list, and you already packed ${numPacked} (${percentage}%)`}
+      </em>
+    </footer>)
+}
